872b9ff382e8fd8ceac4fdc7c389f4a2fc71ff6c
Claire Freehafer <claire.freehafer@gmail.com>
2017-05-04 19:44:36 -0400

    git init

---
+++ index.js
@@ -0,0 +1,10 @@
+axios.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=soccer&api_key=7358a7e094d78889664b99d33aed9e02&format=json')
+	.then((data) => console.log(data))
+
+
+
+// $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=soccer&api_key=7358a7e094d78889664b99d33aed9e02&format=json', function (photos) {
+// 	console.log(photos)
+// })
+
+// console.log($)
